38dcedeca2c20e8c9b79ce7d06241be730cefd95
Joel Macias <macias22@unlv.nevada.edu>
2017-04-25 17:23:07 -0700

    added lab 14

---
+++ lab14/struct.cpp
@@ -0,0 +1,99 @@
+/*
+Lab 13 - structs 
+
+	- Group of elements grouped together under one name 
+	- These data elements are known as members 
+		- Elements can be of diffrent types 
+
+	Example: 
+
+		struct movies {
+			int year;
+			string title;
+			string director; 
+		}; 
+ 
+ struct films {
+ int year;
+ string title;
+ string director;
+ };
+
+	using your struct:
+		movies movieArray[10]               // array of structs
+		movies movie;                       // one struct
+        movie.director = "Steven";
+
+ 
+
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct movies{
+	int year;
+	string title;
+	string director; 
+};
+
+movies Init_Movie(); // returns movie structure
+void Read_Data(movies[], int&); 
+void Print_Movies(movies[], int&);
+int main(){
+
+	movies movieArray[10]; 	// array of movies 
+	int movieCount = 0; 	// keeps track of # of movies in array
+
+	//initialize array of structs
+	for(int i = 0; i < 10; i++){
+		movieArray[i] = Init_Movie(); 	
+	}
+
+	Read_Data(movieArray, movieCount); 
+	Print_Movies(movieArray, movieCount);
+
+
+	return 0;
+}
+
+movies Init_Movie(){
+
+	movies temp; 
+	temp.year = 0;
+	temp.title = "";
+	temp.director = ""; 
+
+	return temp;
+}
+
+
+void Read_Data(movies movieArray[], int& movieCount){
+
+	// store data in respective member field 
+	cin >> movieArray[movieCount].year;
+	cin.get();
+	getline(cin,movieArray[movieCount].title);
+	getline(cin,movieArray[movieCount].director); 
+
+	while(cin){
+			
+		movieCount++; // increment movie count 
+		
+		// store data in respective member field 
+		cin >> movieArray[movieCount].year;
+		cin.get();
+		getline(cin,movieArray[movieCount].title);
+		getline(cin,movieArray[movieCount].director); 
+	}
+
+}
+
+void Print_Movies(movies movieArray[], int& movieCount){
+
+	for(int i = 0; i < movieCount; i++){
+		cout <<movieArray[i].title<<" "<<movieArray[i].director<<" "<<movieArray[i].year<<endl; 
+	}
+}
